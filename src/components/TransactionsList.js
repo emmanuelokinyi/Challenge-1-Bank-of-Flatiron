52b266ac097c8a2ddd5f297c77dd229cb92d3584
Emmanuel <emmanuelokinyi799@gmail.com>
2024-04-29 14:18:23 +0300

    initial commit

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Transaction from "./Transaction";
 
 function TransactionsList() {
+
+  const [transactions, setTransactions] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:8001/transactions")
+            .then(response => response.json())
+            .then(transactions => setTransactions(transactions))
+            .catch(err => console.log(err))
+    }, [])
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +28,16 @@ function TransactionsList() {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}
+        {transactions.map((transactions,index)=>{
+                    return <Transaction
+                    index={index}
+                    date={transactions.date}
+                    category={transactions.category}
+                    description={transactions.description}
+                    amount={transactions.amount}
+                    key={transactions.id}
+                    />
+                })}
       </tbody>
     </table>
   );
